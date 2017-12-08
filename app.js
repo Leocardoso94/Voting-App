ffd7ed72ef795f47f9808c45a658860e4dacef52
Leonardo Cardoso <leocardosoti@gmail.com>
2017-12-08 16:05:25 -0200

    login

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
-const login = require('./routes/login');
 
 const app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
 
 module.exports = router;
